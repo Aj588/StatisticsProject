31958ca5e7ae61a5462db00e07d3ec7c57a91eca
anushajawed <aj588@njit.edu>
2020-03-03 16:23:31 -0500

    fix: addition tests to run

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -6,9 +6,12 @@ from Calculator.Calculator import Calculator
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator()
-        self.statistics = Statistics()
+
 
     def test_calculator_return_sum(self):
         calculator = Calculator()
         result = calculator.Sum(1, 2)
         self.assertEqual(3, result)
+
+if __name__ == '__main__':
+    unittest.main()
