61ca11f0ba5ed291362bae8f82b80939b606ae64
anushajawed <aj588@njit.edu>
2020-03-03 16:43:54 -0500

    task: subtraction

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -13,5 +13,10 @@ class MyTestCase(unittest.TestCase):
         result = calculator.Sum(1, 2)
         self.assertEqual(3, result)
 
+    def test_calculator_return_difference(self):
+        calculator = Calculator()
+        result = calculator.Difference(1, 2)
+        self.assertEqual(-1, result)
+
 if __name__ == '__main__':
     unittest.main()
