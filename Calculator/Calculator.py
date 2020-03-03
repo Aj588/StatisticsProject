5b25665df5d7833fd8fdd64fbf0780985c274516
po47 <po47@njit.edu>
2020-03-03 16:57:35 -0500

    Task: added multiplication

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,6 +1,4 @@
-from MathOperations.addition import Addition
-from MathOperations.subtraction import Subtraction
-
+from MathOperations.multiplication import Multiplication
 
 class Calculator:
     Result = 0
@@ -15,3 +13,6 @@ class Calculator:
     def Difference(self, a, b):
         self.Result = Subtraction.difference(a, b)
         return self.Result
+
+    def Product(self, a, b):
+        self.Result = Multiplication.product(a, b)
