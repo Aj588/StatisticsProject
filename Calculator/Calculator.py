5216a2468518137624bbf254b0b012d03b1863ea
anushajawed <aj588@njit.edu>
2020-03-03 20:37:05 -0500

    task: division

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -8,13 +8,13 @@ class Calculator:
     def __init__(self):
         pass
 
-    def Sum(self, a, b):
+    def sum(self, a, b):
         self.Result = Addition.sum(a, b)
         return self.Result
 
-    def Difference(self, a, b):
+    def difference(self, a, b):
         self.Result = Subtraction.difference(a, b)
         return self.Result
 
-    def Fraction(self, a, b):
+    def fraction(self, a, b):
         self.Result = Division.fraction(a, b)
--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -9,27 +9,27 @@ class MyTestCase(unittest.TestCase):
 
     def test_calculator_return_sum(self):
         calculator = Calculator()
-        result = calculator.Sum(1, 2)
+        result = calculator.sum(1, 2)
         self.assertEqual(result, 3)
 
     def test_calculator_return_difference(self):
         calculator = Calculator()
-        result = calculator.Difference(1, 2)
+        result = calculator.difference(1, 2)
         self.assertEqual(result, -1)
 
     def test_calculator_access_difference_result(self):
         calculator = Calculator()
-        calculator.Difference(1, 2)
+        calculator.difference(1, 2)
         self.assertEqual(calculator.Result, -1)
 
     def test_calculator_access_sum_result(self):
         calculator = Calculator()
-        calculator.Sum(1, 2)
+        calculator.sum(1, 2)
         self.assertEqual(calculator.Result, 3)
 
     def test_calculator_access_fraction_result(self):
         calculator = Calculator()
-        calculator.Fraction(5, 5)
+        calculator.fraction(5, 5)
         self.assertEqual(calculator.Result, 1)
 
 if __name__ == '__main__':
